5ff6ddb2fbd564ebee9667e81dd326ce88f2646c
Ivan Prokic <ivanprokic@yahoo.com>
2025-07-20 22:30:20 -0500

    FIx Comparison Section typings

--- app/components/ComparisonSection.tsx
+++ app/components/ComparisonSection.tsx
@@ -102,7 +102,7 @@ export default function ComparisonSection() {
           return (
             <motion.div
               key={element.id}
-              ref={el => (elementsRefs.current[index] = el)}
+              ref={el => { elementsRefs.current[index] = el; }}
               initial={{ opacity: 0, y: 40 }}
               animate={getAnim(elementsStates[index], forceIntro)}
               transition={{ duration: 0.7, ease: "easeOut" }}
