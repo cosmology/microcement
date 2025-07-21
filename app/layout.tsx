94e5513b9045a81e406e71af441495520eb7f290
Ivan Prokic <ivanprokic@yahoo.com>
2025-07-20 23:24:10 -0500

    Change ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+// import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "next-themes"
 import { Roboto } from 'next/font/google'
 
 const roboto = Roboto({
