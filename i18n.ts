e67e5c606d4a99d953f2a69852eae71a1589fdc1
Ivan Prokic <ivanprokic@yahoo.com>
2025-09-05 08:03:26 +0200

    Adding null check

--- i18n.ts
+++ i18n.ts
@@ -6,7 +6,7 @@ const locales = ['en', 'es', 'sr'];
 
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+  if (!locale || !locales.includes(locale)) notFound();
 
   return {
     locale,
