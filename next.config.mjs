d91c5700933433742f067b97a33c6fd38c3bc17e
Ivan Prokic <ivanprokic@yahoo.com>
2025-09-06 03:08:04 +0200

    converted both API routes from dynamic routing to URL parameters

--- next.config.mjs
+++ next.config.mjs
@@ -4,18 +4,10 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.idealwork.com', // Matches a specific hostname
-        port: '', // Leave empty if no specific port is required
-        pathname: '/wp-content/uploads/**',
-      },
-    ],
-  },
-  // No basePath, no assetPrefix, no output: 'export'
-  // Add any other config you need here
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  trailingSlash: false
 };
 
 export default withNextIntl(nextConfig);
