1437e5c7209f24717f21a4852c10a5776641ab35
Ivan Prokic <ivanprokic@yahoo.com>
2025-09-06 03:24:35 +0200

    revise nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,22 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
-  trailingSlash: false
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.idealwork.com', // Matches a specific hostname
+        port: '', // Leave empty if no specific port is required
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
+    reactStrictMode: true,
+    eslint: { ignoreDuringBuilds: true },
+    typescript: { ignoreBuildErrors: false },
+    trailingSlash: false
+  },
+  // No basePath, no assetPrefix, no output: 'export'
+  // Add any other config you need here
 };
 
 export default withNextIntl(nextConfig);
